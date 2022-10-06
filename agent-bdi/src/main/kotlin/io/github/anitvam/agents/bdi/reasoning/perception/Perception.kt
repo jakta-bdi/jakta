9bdfb529a24bd1ce877c965bdd777657aa9b86b5
Martina <martina.baiardi4@studio.unibo.it>
2022-10-06 12:32:00 +0200

    feat: add Perception implementation

---
+++ agent-bdi/src/main/kotlin/io/github/anitvam/agents/bdi/reasoning/perception/Perception.kt
@@ -0,0 +1,23 @@
+package io.github.anitvam.agents.bdi.reasoning.perception
+
+import io.github.anitvam.agents.bdi.beliefs.BeliefBase
+import io.github.anitvam.agents.bdi.reasoning.perception.impl.EmptyPerception
+
+/**
+ * Component of a BDI Agent that let it perceive the environment.
+ */
+interface Perception {
+
+    /**
+     * Operation done by a BDI Agent at each reasoning cycle iteration.
+     * @return a [BeliefBase] that describes the environment where the agent is situated.
+     */
+    fun percept(): BeliefBase
+
+    companion object {
+        /**
+         * @return a [EmptyPerception]
+         */
+        fun empty(): Perception = EmptyPerception()
+    }
+}
