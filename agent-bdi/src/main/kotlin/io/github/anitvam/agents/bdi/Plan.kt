22aa8f3920ba2dd655f57d5d90500af40b02587f
Martina <martina.baiardi4@studio.unibo.it>
2022-10-07 14:57:54 +0200

    fix: update imports in Plan interface

--- agent-bdi/src/main/kotlin/io/github/anitvam/agents/bdi/Plan.kt
+++ agent-bdi/src/main/kotlin/io/github/anitvam/agents/bdi/Plan.kt
@@ -1,5 +1,8 @@
 package io.github.anitvam.agents.bdi
 
+import io.github.anitvam.agents.bdi.beliefs.BeliefBase
+import io.github.anitvam.agents.bdi.events.Trigger
+
 interface Plan {
     val id: PlanID
     val event: Trigger
