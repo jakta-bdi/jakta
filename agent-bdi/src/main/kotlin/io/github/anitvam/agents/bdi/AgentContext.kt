0e6ddf8009521ea1f8b3bf9c27ed849454d7e420
Martina <martina.baiardi4@studio.unibo.it>
2022-10-07 14:57:20 +0200

    feat: add AgentContext interface and implementation

---
+++ agent-bdi/src/main/kotlin/io/github/anitvam/agents/bdi/AgentContext.kt
@@ -0,0 +1,29 @@
+package io.github.anitvam.agents.bdi
+
+import io.github.anitvam.agents.bdi.beliefs.BeliefBase
+import io.github.anitvam.agents.bdi.events.Events
+import io.github.anitvam.agents.bdi.impl.AgentContextImpl
+
+
+/**
+ * The Context is the actual state of a BDI Agent's structures.
+ */
+interface AgentContext {
+
+    /** [BeliefBase] of the BDI Agent */
+    val beliefBase: BeliefBase
+
+    /** [Events] on which the BDI Agent reacts */
+    val events: Events
+
+    /**
+     * Belief Update Function (BUF): Function that updates the current [BeliefBase] of the Agent with its new
+     * perceptions from the environment.
+     * @return the updated [AgentContext]
+     */
+    fun buf(perceptions: BeliefBase): AgentContext
+
+    companion object {
+        fun of(beliefBase: BeliefBase, events: Events) = AgentContextImpl(beliefBase, events)
+    }
+}
---
+++ agent-bdi/src/main/kotlin/io/github/anitvam/agents/bdi/impl/AgentContextImpl.kt
@@ -0,0 +1,32 @@
+package io.github.anitvam.agents.bdi.impl
+
+import io.github.anitvam.agents.bdi.AgentContext
+import io.github.anitvam.agents.bdi.beliefs.BeliefBase
+import io.github.anitvam.agents.bdi.events.Event
+import io.github.anitvam.agents.bdi.events.Events
+import io.github.anitvam.agents.bdi.events.Trigger
+
+/** Implementation of Agent's [AgentContext] */
+class AgentContextImpl(
+    override val beliefBase: BeliefBase,
+    override val events: Events,
+): AgentContext {
+
+    override fun buf(perceptions: BeliefBase): AgentContext = when (perceptions == beliefBase) {
+        false -> {
+            var newEvents: Events = events
+            // 1. each literal l in p not currently in b is added to b
+            var newBeliefBase = beliefBase.addAll(perceptions.beliefs) {
+                newEvents = newEvents + Event.of(Trigger.ofBeliefBaseAddition(it))
+            }
+            // 2. each literal l in b no longer in p is deleted from b
+            newBeliefBase.beliefs.forEach {
+                if (!perceptions.beliefs.contains(it)) newBeliefBase = newBeliefBase.remove(it) {
+                    b -> newEvents = newEvents + Event.of(Trigger.ofBeliefBaseRemoval(b))
+                }
+            }
+            AgentContext.of(newBeliefBase, newEvents)
+        }
+        else -> this
+    }
+}
