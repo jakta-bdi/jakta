91f15e946997b78ce3d15ac29b44157e57949680
Martina <martina.baiardi4@studio.unibo.it>
2022-10-07 14:58:32 +0200

    feat: add SelectionFunction interface

---
+++ agent-bdi/src/main/kotlin/io/github/anitvam/agents/bdi/reasoning/SelectionFunction.kt
@@ -0,0 +1,16 @@
+package io.github.anitvam.agents.bdi.reasoning
+
+import io.github.anitvam.agents.bdi.Intention
+import io.github.anitvam.agents.bdi.Plan
+import io.github.anitvam.agents.bdi.events.Event
+
+/** A selection function chose element of type T from a collection of them */
+interface SelectionFunction<T> {
+    /** Method that select an element of type [T] from a collection of them */
+    fun select(collection: Iterable<T>): T
+}
+
+// interface MessageSelectionFunction: SelectionFunction<Message>
+interface PlanSelectionFunction : SelectionFunction<Plan>
+interface EventSelectionFunction : SelectionFunction<Event>
+interface IntentionSelectionFunction : SelectionFunction<Intention>
