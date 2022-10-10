91d2edcb7af6b05a7703082236e4b41f9247117b
Martina <martina.baiardi4@studio.unibo.it>
2022-10-10 15:13:15 +0200

    fix: refactor of BeliefBase supporting new typealias of Belief (WIP)

--- agent-bdi/src/main/kotlin/io/github/anitvam/agents/bdi/beliefs/BeliefBase.kt
+++ agent-bdi/src/main/kotlin/io/github/anitvam/agents/bdi/beliefs/BeliefBase.kt
@@ -1,19 +1,11 @@
 package io.github.anitvam.agents.bdi.beliefs
 
-import io.github.anitvam.agents.bdi.Percept
 import io.github.anitvam.agents.bdi.beliefs.impl.BeliefBaseImpl
-import io.github.anitvam.agents.bdi.reasoning.perception.impl.EmptyPerception
 import it.unibo.tuprolog.collections.ClauseMultiSet
 import it.unibo.tuprolog.collections.RetrieveResult
-import it.unibo.tuprolog.theory.Theory
 
-/**
- * A BDI Agent's collection of [Beliefs]
- */
+/** A BDI Agent's collection of [Belief] */
 interface BeliefBase {
-
-    val beliefs: ClauseMultiSet
-
     /**
      * Adds a [Belief] to this [BeliefBase]
      * @param belief: the [Belief] to be added
@@ -24,13 +16,13 @@ interface BeliefBase {
     fun add(belief: Belief, onAdditionPerformed: (addedBelief: Belief) -> Unit = {}): BeliefBase
 
     /**
-     * Adds all the given [Belief] to this [BeliefBase]
-     * @param beliefs: the collection of [Belief] to be added
+     * Adds all the given beliefs into this [BeliefBase]
+     * @param beliefs: beliefs to be added
      * @param onAdditionPerformed: the callback that will be invoked when each addition is performed, it takes the
      * added belief as a parameter. By default, it does nothing.
      * @return the updated [BeliefBase]
      **/
-    fun addAll(beliefs: Iterable<Belief>, onAdditionPerformed: (addedBelief: Belief) -> Unit = {}): BeliefBase
+    fun addAll(beliefs: BeliefBase, onAdditionPerformed: (addedBelief: Belief) -> Unit = {}): BeliefBase
 
     /** Retrieves the first unifying [Belief] from this [BeliefBase] as a [RetrieveResult]**/
     fun retrieve(belief: Belief): RetrieveResult<out ClauseMultiSet>
@@ -42,13 +34,42 @@ interface BeliefBase {
      * Removes a [Belief] from the [BeliefBase]
      * @param belief: the [Belief] to be removed
      * @param onRemovalPerformed: the callback that will be invoked when the removal is performed, it takes the
-     * added belief as a parameter. By default, it does nothing.
+     * removed belief as a parameter. By default, it does nothing.
      * @return the updated [BeliefBase]
      */
     fun remove(belief: Belief, onRemovalPerformed: (removedBelief: Belief) -> Unit = {}): BeliefBase
 
+    /**
+     * Removes all the specified beliefs from this [BeliefBase]
+     * @param beliefs: beliefs to be removed
+     * @param onRemovalPerformed: the callback that will be invoked when each one removal is performed, it takes the
+     * removed belief as a parameter. By default, it does nothing.
+     * @return the updated [BeliefBase]
+     */
+    fun removeAll(beliefs: BeliefBase, onRemovalPerformed: (removedBelief: Belief) -> Unit = {}): BeliefBase
+
+    /**
+     * Executes an action for each element of this [BeliefBase]
+     * @param action: the action to be executed
+     */
+    fun forEachBelief(action: (Belief) -> Unit)
+
+    /**
+     * Returns a [BeliefBase] containing only elements matching the given filter.
+     * @param filter: the predicate
+     * @return a new [BeliefBase] with the elements matching the filter
+     */
+    fun filter(filter: (Belief) -> Boolean): BeliefBase
+
     companion object {
+        /** @return an empty [BeliefBase] */
         fun empty(): BeliefBase = BeliefBaseImpl(ClauseMultiSet.empty())
+
+        /**
+         * Generates a [BeliefBase] from a collection of [Belief]
+         * @param beliefs: the [Iterable] of [Belief] the [BeliefBase] will be composed of
+         * @return the new [BeliefBase]
+         */
         fun of(beliefs: Iterable<Belief>): BeliefBase = BeliefBaseImpl(ClauseMultiSet.of(beliefs))
     }
 }
--- agent-bdi/src/main/kotlin/io/github/anitvam/agents/bdi/beliefs/impl/BeliefBaseImpl.kt
+++ agent-bdi/src/main/kotlin/io/github/anitvam/agents/bdi/beliefs/impl/BeliefBaseImpl.kt
@@ -4,8 +4,9 @@ import io.github.anitvam.agents.bdi.beliefs.Belief
 import io.github.anitvam.agents.bdi.beliefs.BeliefBase
 import it.unibo.tuprolog.collections.ClauseMultiSet
 import it.unibo.tuprolog.collections.RetrieveResult
+import it.unibo.tuprolog.core.Clause
 
-internal class BeliefBaseImpl (override val beliefs: ClauseMultiSet): BeliefBase {
+internal class BeliefBaseImpl(private val beliefs: ClauseMultiSet): BeliefBase {
     override fun add(belief: Belief, onAdditionPerformed: (addedBelief: Belief) -> Unit) =
         when (beliefs.count(belief)) {
             // There's no Belief that unify the param inside the MultiSet, so it's inserted
@@ -17,9 +18,9 @@ internal class BeliefBaseImpl (override val beliefs: ClauseMultiSet): BeliefBase
             else -> this
         }
 
-    override fun addAll(beliefs: Iterable<Belief>, onAdditionPerformed: (addedBelief: Belief) -> Unit): BeliefBase {
+    override fun addAll(beliefs: BeliefBase, onAdditionPerformed: (addedBelief: Belief) -> Unit): BeliefBase {
         var bSet: BeliefBase = this
-        beliefs.forEach { bSet = add(it, onAdditionPerformed) }
+        beliefs.forEachBelief { bSet = add(it, onAdditionPerformed) }
         return bSet
     }
 
@@ -27,14 +28,28 @@ internal class BeliefBaseImpl (override val beliefs: ClauseMultiSet): BeliefBase
 
     override fun equals(other: Any?) = beliefs == other
 
+    override fun forEachBelief(action: (Belief) -> Unit) = beliefs.forEach { action(it as Belief) }
+    // Downcast orribile. Tengo ClauseMultiSet o Collection<Belief>??? In teoria però so già che dentro avrò solo Belief perchè li vincolo da interfaccia
+
+    override fun filter(filter: (Belief) -> Boolean): BeliefBase {
+        var bSet: BeliefBase = this
+        forEachBelief { if (!filter(it)) bSet = remove(it) }
+        return bSet
+    }
     override fun retrieve(belief: Belief): RetrieveResult<out ClauseMultiSet> = beliefs.retrieve(belief)
 
     override fun retrieveAll(belief: Belief): RetrieveResult<out ClauseMultiSet> = beliefs.retrieveAll(belief)
 
     // DA TESTARE BENE - NON SONO SICURA FUNZIONI
     override fun remove(belief: Belief, onRemovalPerformed: (removedBelief: Belief) -> Unit): BeliefBase {
-        beliefs.filter { it == belief }.forEach { onRemovalPerformed(it) }
-        return BeliefBase.of(beliefs.filter { it != belief })
+        beliefs.filter { it == belief }.forEach { onRemovalPerformed(it as Belief) }
+        return filter { it != belief }
+    }
+
+    override fun removeAll(beliefs: BeliefBase, onRemovalPerformed: (removedBelief: Belief) -> Unit): BeliefBase {
+        var bSet: BeliefBase = this
+        beliefs.forEachBelief { bSet = remove(it, onRemovalPerformed) }
+        return bSet
     }
 
 }
