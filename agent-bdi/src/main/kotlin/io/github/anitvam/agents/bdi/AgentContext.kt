e09d7d41c4cad40599638cb616dd99e6823d5fec
Martina <martina.baiardi4@studio.unibo.it>
2022-10-13 15:00:23 +0200

    feat: add Perception and PlanLibrary components in AgentContext.kt

--- agent-bdi/src/main/kotlin/io/github/anitvam/agents/bdi/AgentContext.kt
+++ agent-bdi/src/main/kotlin/io/github/anitvam/agents/bdi/AgentContext.kt
@@ -2,7 +2,11 @@ package io.github.anitvam.agents.bdi
 
 import io.github.anitvam.agents.bdi.beliefs.BeliefBase
 import io.github.anitvam.agents.bdi.events.EventQueue
+import io.github.anitvam.agents.bdi.events.Event
 import io.github.anitvam.agents.bdi.impl.AgentContextImpl
+import io.github.anitvam.agents.bdi.plans.PlanLibrary
+import io.github.anitvam.agents.bdi.plans.Plan
+import io.github.anitvam.agents.bdi.reasoning.perception.Perception
 
 /**
  * The Context is the actual state of a BDI Agent's structures.
@@ -12,8 +16,14 @@ interface AgentContext {
     /** [BeliefBase] of the BDI Agent */
     val beliefBase: BeliefBase
 
-    /** [Events] on which the BDI Agent reacts */
+    /** [Event]s on which the BDI Agent reacts */
     val events: EventQueue
+
+    /** Component of the Agent that let it perceive the environment */
+    val perception: Perception
+
+    /** [Plan]s collection of the BDI Agent */
+    val planLibrary : PlanLibrary
 
     /**
      * Belief Update Function (BUF): Function that updates the current [BeliefBase] of the Agent with its new
--- agent-bdi/src/main/kotlin/io/github/anitvam/agents/bdi/impl/AgentContextImpl.kt
+++ agent-bdi/src/main/kotlin/io/github/anitvam/agents/bdi/impl/AgentContextImpl.kt
@@ -5,23 +5,27 @@ import io.github.anitvam.agents.bdi.beliefs.BeliefBase
 import io.github.anitvam.agents.bdi.events.Event
 import io.github.anitvam.agents.bdi.events.EventQueue
 import io.github.anitvam.agents.bdi.events.Trigger
+import io.github.anitvam.agents.bdi.plans.PlanLibrary
+import io.github.anitvam.agents.bdi.reasoning.perception.Perception
 
 /** Implementation of Agent's [AgentContext] */
 class AgentContextImpl(
     override val beliefBase: BeliefBase,
     override val events: EventQueue,
+    override val planLibrary: PlanLibrary,
+    override val perception: Perception,
 ): AgentContext {
 
     override fun buf(perceptions: BeliefBase): AgentContext = when (perceptions == beliefBase) {
         false -> {
             var newEvents = events
             // 1. each literal l in p not currently in b is added to b
-            var newBeliefBase = beliefBase.addAll(perceptions.beliefs) {
+            var newBeliefBase = beliefBase.addAll(perceptions) {
                 newEvents = newEvents + Event.of(Trigger.ofBeliefBaseAddition(it))
             }
             // 2. each literal l in b no longer in p is deleted from b
-            newBeliefBase.beliefs.forEach {
-                if (!perceptions.beliefs.contains(it)) newBeliefBase = newBeliefBase.remove(it) {
+            newBeliefBase.forEachBelief {
+                if (!perceptions.contains(it)) newBeliefBase = newBeliefBase.remove(it) {
                     b -> newEvents = newEvents + Event.of(Trigger.ofBeliefBaseRemoval(b))
                 }
             }
