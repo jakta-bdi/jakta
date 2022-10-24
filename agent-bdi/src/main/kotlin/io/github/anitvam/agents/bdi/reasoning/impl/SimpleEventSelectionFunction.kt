028fc03a639aeec3e165c5cd2b3afc1e9a7cfade
Martina <martina.baiardi4@studio.unibo.it>
2022-10-24 18:00:13 +0200

    fix: refactor of SelectionFunction.kt and remove outdated files

--- agent-bdi/src/main/kotlin/io/github/anitvam/agents/bdi/reasoning/impl/SimpleEventSelectionFunction.kt
+++
@@ -1,9 +0,0 @@
-package io.github.anitvam.agents.bdi.reasoning.impl
-
-import io.github.anitvam.agents.bdi.events.Event
-import io.github.anitvam.agents.bdi.reasoning.EventSelectionFunction
-
-/** Simple implementation of [EventSelectionFunction] that takes the first element of the queue */
-class SimpleEventSelectionFunction : EventSelectionFunction {
-    override fun select(collection: Iterable<Event>): Event = collection.last()
-}
--- agent-bdi/src/main/kotlin/io/github/anitvam/agents/bdi/reasoning/impl/SimpleIntentionSelectionFunction.kt
+++
@@ -1,8 +0,0 @@
-package io.github.anitvam.agents.bdi.reasoning.impl
-
-import io.github.anitvam.agents.bdi.intentions.Intention
-import io.github.anitvam.agents.bdi.reasoning.IntentionSelectionFunction
-
-class SimpleIntentionSelectionFunction : IntentionSelectionFunction{
-    override fun select(collection: Iterable<Intention>): Intention = TODO()
-}
