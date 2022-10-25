b66707f21632b4a47ea0b20c7246a7742a238fd6
Martina <martina.baiardi4@studio.unibo.it>
2022-10-25 12:38:57 +0200

    feat: add implementation of IntentionPool.kt and add other methods to handle it

--- agent-bdi/src/main/kotlin/io/github/anitvam/agents/bdi/intentions/IntentionPool.kt
+++ agent-bdi/src/main/kotlin/io/github/anitvam/agents/bdi/intentions/IntentionPool.kt
@@ -1,5 +1,15 @@
 package io.github.anitvam.agents.bdi.intentions
 
+import io.github.anitvam.agents.bdi.intentions.impl.IntentionPoolImpl
+
 interface IntentionPool : Map<IntentionID, Intention> {
     fun update(intention: Intention): IntentionPool
+
+    fun nextIntention(): Intention
+
+    fun pop(): IntentionPool
+
+    companion object {
+        fun empty() : IntentionPool = IntentionPoolImpl()
+    }
 }
---
+++ agent-bdi/src/main/kotlin/io/github/anitvam/agents/bdi/intentions/impl/IntentionPoolImpl.kt
@@ -0,0 +1,17 @@
+package io.github.anitvam.agents.bdi.intentions.impl
+
+import io.github.anitvam.agents.bdi.intentions.Intention
+import io.github.anitvam.agents.bdi.intentions.IntentionID
+import io.github.anitvam.agents.bdi.intentions.IntentionPool
+
+internal class IntentionPoolImpl(
+    from : Map<IntentionID, Intention> = emptyMap(),
+) : IntentionPool, LinkedHashMap<IntentionID, Intention>(from) {
+
+    override fun update(intention: Intention) =
+        IntentionPoolImpl(this + Pair(intention.id, intention))
+
+    override fun nextIntention() : Intention = this.entries.iterator().next().value
+
+    override fun pop(): IntentionPool = IntentionPoolImpl(this - nextIntention().id)
+}
