b2514dc8f403d003b543ce2625334185c39ed647
Martina <martina.baiardi4@studio.unibo.it>
2023-01-23 17:45:50 +0100

    fix: deleted unused package

--- agent-bdi/src/main/kotlin/io/github/anitvam/agents/bdi/reasoning/SelectionFunction.kt
+++
@@ -1,7 +0,0 @@
-package io.github.anitvam.agents.bdi.reasoning
-
-/** A selection function chose element of type T from a collection of them */
-fun interface SelectionFunction<T> {
-    /** Method that select an element of type [T] from a collection of them */
-    fun select(collection: Iterable<T>): T
-}
