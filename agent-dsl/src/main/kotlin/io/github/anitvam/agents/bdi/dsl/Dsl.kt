a3684f6be0b2f34df2e102d4e67458237ffa21bd
Martina <martina.baiardi4@studio.unibo.it>
2023-04-04 17:22:01 +0200

    feat: enable external definition for environment and plans

--- agent-dsl/src/main/kotlin/io/github/anitvam/agents/bdi/dsl/Dsl.kt
+++ agent-dsl/src/main/kotlin/io/github/anitvam/agents/bdi/dsl/Dsl.kt
@@ -5,12 +5,29 @@ import io.github.anitvam.agents.bdi.dsl.actions.ExternalActionScope
 import io.github.anitvam.agents.bdi.dsl.actions.ExternalActionsScope
 import io.github.anitvam.agents.bdi.dsl.actions.InternalActionScope
 import io.github.anitvam.agents.bdi.dsl.actions.InternalActionsScope
+import io.github.anitvam.agents.bdi.dsl.environment.EnvironmentScope
+import io.github.anitvam.agents.bdi.dsl.plans.PlansScope
+import io.github.anitvam.agents.bdi.environment.Environment
+import io.github.anitvam.agents.bdi.plans.Plan
+import it.unibo.tuprolog.core.Struct
+import it.unibo.tuprolog.core.Term
 
 fun mas(f: MasScope.() -> Unit): Mas =
     MasScope().also(f).build()
-
 fun internalAction(name: String, arity: Int, f: InternalActionScope.() -> Unit) =
     InternalActionsScope().newAction(name, arity, f)
 
 fun externalAction(name: String, arity: Int, f: ExternalActionScope.() -> Unit) =
     ExternalActionsScope().newAction(name, arity, f)
+
+fun environment(f: EnvironmentScope.() -> Unit): Environment {
+    return EnvironmentScope().also(f).build()
+}
+
+fun plans(f: PlansScope.() -> Unit): Iterable<Plan> = PlansScope().also(f).build()
+
+operator fun String.invoke(vararg terms: Term): Struct =
+    Struct.of(this, *terms)
+
+operator fun String.invoke(terms: List<Term>): Struct =
+    Struct.of(this, terms)
