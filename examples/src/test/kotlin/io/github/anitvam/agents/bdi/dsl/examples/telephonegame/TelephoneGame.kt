d9d15e93d4dd884c8453d8b80e164f77aa25272f
Danilo Pianini <danilo.pianini@unibo.it>
2023-04-06 13:08:17 +0200

    style: beautify the telephone example, showcase string literal purification via delegate properties

---
+++ examples/src/test/kotlin/io/github/anitvam/agents/bdi/dsl/examples/telephonegame/TelephoneGame.kt
@@ -0,0 +1,55 @@
+package io.github.anitvam.agents.bdi.dsl.examples.telephonegame
+
+import io.github.anitvam.agents.bdi.Message
+import io.github.anitvam.agents.bdi.dsl.examples.telephonegame.TelephoneProgramLiterals.count
+import io.github.anitvam.agents.bdi.dsl.examples.telephonegame.TelephoneProgramLiterals.print
+import io.github.anitvam.agents.bdi.dsl.examples.telephonegame.TelephoneProgramLiterals.receiver
+import io.github.anitvam.agents.bdi.dsl.examples.telephonegame.TelephoneProgramLiterals.self
+import io.github.anitvam.agents.bdi.dsl.examples.telephonegame.TelephoneProgramLiterals.send
+import io.github.anitvam.agents.bdi.dsl.examples.telephonegame.TelephoneProgramLiterals.source
+import io.github.anitvam.agents.bdi.dsl.examples.telephonegame.TelephoneProgramLiterals.start
+import io.github.anitvam.agents.bdi.dsl.mas
+import io.github.anitvam.agents.bdi.messages.Tell
+import it.unibo.tuprolog.core.Atom
+import it.unibo.tuprolog.core.Struct
+
+fun main() {
+    val nAgents = 5
+    val nMessages = 10
+    mas {
+        environment {
+            actions {
+                action(send, 2) {
+                    val receiver: Atom = argument(0)
+                    val message: Struct = argument(1)
+                    this.sendMessage(receiver.value, Message(this.sender, Tell, message))
+                }
+            }
+        }
+
+        repeat(nAgents) {id ->
+            agent("ag$id") {
+                beliefs {
+                    fact(receiver("ag${(id + 1) % nAgents}"))
+                }
+                if (id == 0) goals { achieve(start) }
+                plans {
+                    + achieve(start) onlyIf { receiver(source(self), R) } then {
+                        iact(print("Starting..."))
+                        act(send(R, count(1)))
+                    }
+                    + count(source(`_`), X) onlyIf {
+                        (X lowerThan nMessages) and (M `is` (X + 1)) and receiver(source(self), R)
+                    } then {
+                        iact(print("Sending ", M))
+                        act(send(R, count(M)))
+                    }
+                    + count(source(`_`), X) onlyIf { X greaterThanOrEqualsTo nMessages } then {
+                        iact(print("Done!"))
+                    }
+                }
+            }
+        }
+    }.start()
+}
+
---
+++ examples/src/test/kotlin/io/github/anitvam/agents/bdi/dsl/examples/telephonegame/TelephoneProgramLiterals.kt
@@ -0,0 +1,19 @@
+package io.github.anitvam.agents.bdi.dsl.examples.telephonegame
+
+import kotlin.reflect.KProperty
+
+object OwnName {
+    operator fun getValue(thisRef: Any?, property: KProperty<*>): String {
+        return property.name
+    }
+}
+
+object TelephoneProgramLiterals {
+    val receiver: String by OwnName
+    val source: String by OwnName
+    val start: String by OwnName
+    val self: String by OwnName
+    val print: String by OwnName
+    val send: String by OwnName
+    val count: String by OwnName
+}
