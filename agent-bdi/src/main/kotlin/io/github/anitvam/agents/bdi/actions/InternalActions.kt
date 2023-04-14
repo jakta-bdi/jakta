d21a5614dcf071a40c5965c7628c87ee6bcc780d
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2023-04-14 12:43:13 +0200

    chore: rename default internal actions

--- agent-bdi/src/main/kotlin/io/github/anitvam/agents/bdi/actions/InternalActions.kt
+++ agent-bdi/src/main/kotlin/io/github/anitvam/agents/bdi/actions/InternalActions.kt
@@ -3,45 +3,43 @@ package io.github.anitvam.agents.bdi.actions
 import io.github.anitvam.agents.bdi.actions.impl.AbstractInternalAction
 import it.unibo.tuprolog.core.Substitution
 
-interface InternalActions {
-    companion object {
-        val PRINT: InternalAction = object : AbstractInternalAction("print", 2) {
-            override fun action(request: InternalRequest) =
-                println("[${request.agent.name}] ${request.arguments.joinToString(separator = " ")}")
-        }
-
-        val FAIL: InternalAction = object : AbstractInternalAction("fail", 0) {
-            override fun action(request: InternalRequest) {
-                result = Substitution.failed()
-            }
-        }
-
-        val STOP: InternalAction = object : AbstractInternalAction("stop", 0) {
-            override fun action(request: InternalRequest) {
-                stopAgent()
-            }
-        }
-
-        val PAUSE: InternalAction = object : AbstractInternalAction("pause", 0) {
-            override fun action(request: InternalRequest) {
-                pauseAgent()
-            }
-        }
-
-        val SLEEP: InternalAction = object : AbstractInternalAction("sleep", 1) {
-            override fun action(request: InternalRequest) {
-                if (request.arguments[0].isInteger) {
-                    Thread.sleep(request.arguments[0].castToInteger().value.toLong())
-                }
-            }
-        }
-
-        fun default() = mapOf(
-            PRINT.signature.name to PRINT,
-            FAIL.signature.name to FAIL,
-            STOP.signature.name to STOP,
-            PAUSE.signature.name to PAUSE,
-            SLEEP.signature.name to SLEEP,
-        )
+object InternalActions {
+    object Print : AbstractInternalAction("print", 2) {
+        override fun action(request: InternalRequest) =
+            println("[${request.agent.name}] ${request.arguments.joinToString(separator = " ")}")
     }
+
+    object Fail : AbstractInternalAction("fail", 0) {
+        override fun action(request: InternalRequest) {
+            result = Substitution.failed()
+        }
+    }
+
+    object Stop : AbstractInternalAction("stop", 0) {
+        override fun action(request: InternalRequest) {
+            stopAgent()
+        }
+    }
+
+    object Pause : AbstractInternalAction("pause", 0) {
+        override fun action(request: InternalRequest) {
+            pauseAgent()
+        }
+    }
+
+    object Sleep : AbstractInternalAction("sleep", 1) {
+        override fun action(request: InternalRequest) {
+            if (request.arguments[0].isInteger) {
+                Thread.sleep(request.arguments[0].castToInteger().value.toLong())
+            }
+        }
+    }
+
+    fun default() = mapOf(
+        Print.signature.name to Print,
+        Fail.signature.name to Fail,
+        Stop.signature.name to Stop,
+        Pause.signature.name to Pause,
+        Sleep.signature.name to Sleep,
+    )
 }
