99b901c5ee89610e6f2136418c662553432bbba8
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2023-04-14 12:43:59 +0200

    fix: allow for rule{} and fact{} syntax in BeliefScope

--- agent-dsl/src/main/kotlin/io/github/anitvam/agents/bdi/dsl/beliefs/BeliefsScope.kt
+++ agent-dsl/src/main/kotlin/io/github/anitvam/agents/bdi/dsl/beliefs/BeliefsScope.kt
@@ -3,22 +3,30 @@ package io.github.anitvam.agents.bdi.dsl.beliefs
 import io.github.anitvam.agents.bdi.beliefs.Belief
 import io.github.anitvam.agents.bdi.beliefs.BeliefBase
 import io.github.anitvam.agents.bdi.dsl.Builder
+import it.unibo.tuprolog.core.Fact
 import it.unibo.tuprolog.core.Rule
 import it.unibo.tuprolog.core.Struct
 import it.unibo.tuprolog.dsl.LogicProgrammingScope
 
-class BeliefsScope : Builder<BeliefBase>, LogicProgrammingScope by LogicProgrammingScope.empty() {
+class BeliefsScope(
+    private val lp: LogicProgrammingScope = LogicProgrammingScope.empty()
+) : Builder<BeliefBase>, LogicProgrammingScope by lp {
 
     private val beliefs = mutableListOf<Belief>()
-
-    private val lp = LogicProgrammingScope.empty()
-
     fun fact(struct: Struct) {
         val belief: Belief = Belief.fromSelfSource(struct.freshCopy())
         beliefs.add(belief)
     }
 
+    override fun rule(function: LogicProgrammingScope.() -> Any): Rule {
+        return lp.rule(function).also { rule(it) }
+    }
+
     fun fact(atom: String) = fact(atomOf(atom))
+
+    override fun fact(function: LogicProgrammingScope.() -> Any): Fact {
+        return lp.fact(function).also { fact(it) }
+    }
 
     fun rule(rule: Rule) {
         @Suppress("NAME_SHADOWING")
