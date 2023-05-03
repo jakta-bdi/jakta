dd460dabd48d53a0e1b24e57c88a53613a0c381f
Martina Baiardi <martina.baiardi4@studio.unibo.it>
2023-05-03 12:05:02 +0200

    feat: add Jakta method for ASL syntax translation

--- jakta-bdi/src/main/kotlin/it/unibo/jakta/agents/bdi/Jakta.kt
+++ jakta-bdi/src/main/kotlin/it/unibo/jakta/agents/bdi/Jakta.kt
@@ -2,6 +2,7 @@ package it.unibo.jakta.agents.bdi
 
 import it.unibo.tuprolog.core.Clause
 import it.unibo.tuprolog.core.Struct
+import it.unibo.tuprolog.core.TermFormatter
 import it.unibo.tuprolog.core.operators.Operator
 import it.unibo.tuprolog.core.operators.OperatorSet
 import it.unibo.tuprolog.core.operators.Specifier
@@ -18,7 +19,30 @@ object Jakta {
     )
 
     private val parser = TermParser.withOperators(OperatorSet.DEFAULT + operators)
-
     fun parseStruct(string: String): Struct = parser.parseStruct(string)
     fun parseClause(string: String): Clause = parser.parseClause(string)
+
+    fun printAslSyntax(agent: Agent, prettyFormatted: Boolean = true) {
+        println("% ${agent.name}")
+        for (belief in agent.context.beliefBase) {
+            if (prettyFormatted) {
+                val formatter = TermFormatter.prettyExpressions(operatorSet = OperatorSet.DEFAULT + Jakta.operators)
+                println(formatter.format(belief.rule))
+            } else {
+                println(belief.rule)
+            }
+        }
+        for (plan in agent.context.planLibrary.plans) {
+            var trigger = plan.trigger.value.toString()
+            var guard = plan.guard.toString()
+            var body = plan.goals.joinToString("; ") { it.value.toString() }
+            if (prettyFormatted) {
+                val formatter = TermFormatter.prettyExpressions(operatorSet = OperatorSet.DEFAULT + Jakta.operators)
+                trigger = formatter.format(plan.trigger.value)
+                guard = formatter.format(plan.guard)
+                body = plan.goals.joinToString("; ") { formatter.format(it.value) }
+            }
+            println("+!$trigger : $guard <- $body")
+        }
+    }
 }
