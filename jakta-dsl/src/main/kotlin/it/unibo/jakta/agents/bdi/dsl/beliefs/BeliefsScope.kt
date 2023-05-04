0d8366f43bcba13de10c10a28a3612f519553d6a
Martina Baiardi <martina.baiardi4@studio.unibo.it>
2023-05-04 11:08:51 +0200

    fix: fix facts generation in beliefs dsl

--- jakta-dsl/src/main/kotlin/it/unibo/jakta/agents/bdi/dsl/beliefs/BeliefsScope.kt
+++ jakta-dsl/src/main/kotlin/it/unibo/jakta/agents/bdi/dsl/beliefs/BeliefsScope.kt
@@ -7,6 +7,7 @@ import it.unibo.tuprolog.core.Fact
 import it.unibo.tuprolog.core.Rule
 import it.unibo.tuprolog.core.Struct
 import it.unibo.tuprolog.dsl.LogicProgrammingScope
+import it.unibo.tuprolog.solve.stdlib.rule.SetPrologFlag.head
 
 class BeliefsScope(
     private val lp: LogicProgrammingScope = LogicProgrammingScope.empty()
@@ -15,24 +16,24 @@ class BeliefsScope(
     private val beliefs = mutableListOf<Belief>()
 
     fun fact(struct: Struct) {
-        val belief: Belief = Belief.wrap(struct.freshCopy())
+        val s = struct.castToRule().head
+        val belief: Belief = Belief.wrap(s.freshCopy())
         beliefs.add(belief)
     }
-
-    override fun rule(function: LogicProgrammingScope.() -> Any): Rule {
-        return lp.rule(function).also { rule(it) }
-    }
-
-    fun fact(atom: String) = fact(atomOf(atom))
 
     override fun fact(function: LogicProgrammingScope.() -> Any): Fact {
         return lp.fact(function).also { fact(it) }
     }
 
+    fun fact(atom: String) = fact(atomOf(atom))
+
+    override fun rule(function: LogicProgrammingScope.() -> Any): Rule {
+        return lp.rule(function).also { rule(it) }
+    }
+
     fun rule(rule: Rule) {
-        @Suppress("NAME_SHADOWING")
-        val rule = rule.freshCopy()
-        val belief: Belief = Belief.wrap(rule.head, rule.bodyItems)
+        val freshRule = rule.freshCopy()
+        val belief: Belief = Belief.wrap(freshRule.head, freshRule.bodyItems)
         beliefs.add(belief)
     }
 
--- jakta-dsl/src/test/kotlin/it/unibo/jakta/agents/bdi/dsl/TestDSL.kt
+++ jakta-dsl/src/test/kotlin/it/unibo/jakta/agents/bdi/dsl/TestDSL.kt
@@ -1,5 +1,6 @@
 package it.unibo.jakta.agents.bdi.dsl
 
+import it.unibo.jakta.agents.bdi.dsl.beliefs.fromSelf
 import it.unibo.tuprolog.core.Struct
 
 fun main() {
@@ -15,16 +16,19 @@ fun main() {
         }
         agent("pippo") {
             beliefs {
-                fact { "good"("ice-cream") }
+                val first = "cell"(A, B, C)
+                val second = "cell"(X, Y, Z)
+                rule { first.fromSelf impliedBy second }
+                fact { "good"("ice-cream").fromSelf }
             }
             goals {
                 achieve("eat"("ice-cream"))
             }
             plans {
-                + achieve("eat"("ice-cream")) onlyIf { "good"("source"("self"), "ice-cream") } then {
+                + achieve("eat"("ice-cream")) onlyIf { "good"("ice-cream").fromSelf } then {
                     execute("pluto"("YUMMY!"))
                 }
             }
         }
-    }.start()
+    }.start(true)
 }
