8081655b55ab758a9dff011437fff8a42850a062
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-11-08 14:05:45 +0000

    feat: add factory for building SerializableMessage

--- jakta-distributed/jakta-distributed-common/src/main/kotlin/it/unibo/jakta/agents/distributed/common/SerializableMessage.kt
+++ jakta-distributed/jakta-distributed-common/src/main/kotlin/it/unibo/jakta/agents/distributed/common/SerializableMessage.kt
@@ -1,5 +1,6 @@
 package it.unibo.jakta.agents.distributed.common
 
+import it.unibo.jakta.agents.bdi.messages.Message
 import it.unibo.jakta.agents.bdi.messages.MessageType
 import it.unibo.tuprolog.core.Struct
 import kotlinx.serialization.Serializable
@@ -9,4 +10,10 @@ data class SerializableMessage(
     val from: String,
     val type: MessageType,
     val value: Struct,
-)
+) {
+    companion object {
+        fun fromMessage(message: Message): SerializableMessage {
+            return SerializableMessage(message.from, message.type, message.value)
+        }
+    }
+}
