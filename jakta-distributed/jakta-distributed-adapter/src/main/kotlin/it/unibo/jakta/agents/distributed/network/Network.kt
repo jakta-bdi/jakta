0decc8a8e3db43f81ffd7ec0f6ce06eae4128d9f
LEONARDO MICELLI <leonardomicelli@gmail.com>
2023-11-10 17:32:42 +0100

    feat: add cluster subscription to network inteface

--- jakta-distributed/jakta-distributed-adapter/src/main/kotlin/it/unibo/jakta/agents/distributed/network/Network.kt
+++ jakta-distributed/jakta-distributed-adapter/src/main/kotlin/it/unibo/jakta/agents/distributed/network/Network.kt
@@ -3,9 +3,14 @@ package it.unibo.jakta.agents.distributed.network
 import it.unibo.jakta.agents.bdi.actions.effects.BroadcastMessage
 import it.unibo.jakta.agents.bdi.actions.effects.EnvironmentChange
 import it.unibo.jakta.agents.bdi.actions.effects.SendMessage
+import it.unibo.jakta.agents.utils.Promise
 
 /** A network is a component that allows agents to communicate with each other.*/
 interface Network {
+    /**
+     * Subscribes the DMas to the cluster, so that it can receive messages from other DMas.
+     */
+    fun subscribeToCluster(): Promise<Unit>
     /**
      * Sends a message with a specified recipient.
      */
