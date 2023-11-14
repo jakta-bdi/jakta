050cf5fdd3036d05c3e6d474cfcdfdf2ed23ca1d
Angela Cortecchia <angela.cortecchia@hotmail.com>
2023-11-14 16:40:28 +0100

    feat: create client web sockets

--- jakta-distributed/jakta-distributed-client/src/main/kotlin/it/unibo/jakta/agents/distributed/client/Application.kt
+++
@@ -1,3 +0,0 @@
-package it.unibo.jakta.agents.distributed.client
-
-class Application
---
+++ jakta-distributed/jakta-distributed-client/src/main/kotlin/it/unibo/jakta/agents/distributed/client/Client.kt
@@ -0,0 +1,13 @@
+package it.unibo.jakta.agents.distributed.client
+
+import it.unibo.jakta.agents.distributed.client.impl.WebSocketsClient
+
+interface Client {
+    fun publish(topic: String, data: Any)
+    fun subscribe(topic: String)
+    val incomingData: MutableMap<String, Any>
+
+    companion object {
+        fun webSocketClient(): Client = WebSocketsClient()
+    }
+}
