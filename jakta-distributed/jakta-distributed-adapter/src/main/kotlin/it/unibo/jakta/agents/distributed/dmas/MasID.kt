0920bccfe6499e0786b137777397253bbcb5c9f9
LEONARDO MICELLI <leonardomicelli@gmail.com>
2023-11-16 15:08:03 +0100

    chore: remove duplicate file

--- jakta-distributed/jakta-distributed-adapter/src/main/kotlin/it/unibo/jakta/agents/distributed/dmas/MasID.kt
+++
@@ -1,7 +0,0 @@
-package it.unibo.jakta.agents.distributed.dmas
-
-data class MasID(val id: String = generateId()) {
-    companion object {
-        private fun generateId(): String = java.util.UUID.randomUUID().toString()
-    }
-}
