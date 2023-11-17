06df0912855ba10aa1ee0dc63e52c0024989f490
LEONARDO MICELLI <leonardomicelli@gmail.com>
2023-11-17 11:44:36 +0100

    chore: remove useless files

--- jakta-distributed-broker/src/main/kotlin/it/unibo/jakta/agents/bdi/distributed_broker/plugins/Routing.kt
+++
@@ -1,9 +0,0 @@
-package it.unibo.jakta.agents.bdi.distributed_broker.plugins
-
-import io.ktor.server.application.*
-import io.ktor.server.routing.*
-
-fun Application.configureRouting() {
-    routing {
-    }
-}
