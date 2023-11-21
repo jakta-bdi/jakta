298fb2456023556ebc94347bc5c4a9b4c945f11f
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-11-21 15:47:13 +0000

    fix: now SerializableMessage is serialized correctly

--- jakta-distributed/jakta-distributed-common/src/main/kotlin/it/unibo/jakta/agents/distributed/common/SerializableMessage.kt
+++ jakta-distributed/jakta-distributed-common/src/main/kotlin/it/unibo/jakta/agents/distributed/common/SerializableMessage.kt
@@ -1,19 +1,30 @@
 package it.unibo.jakta.agents.distributed.common
 
+import it.unibo.jakta.agents.bdi.messages.Achieve
 import it.unibo.jakta.agents.bdi.messages.Message
-import it.unibo.jakta.agents.bdi.messages.MessageType
+import it.unibo.jakta.agents.bdi.messages.Tell
 import it.unibo.tuprolog.core.Struct
+import it.unibo.tuprolog.core.parsing.parse
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class SerializableMessage(
     val from: String,
-    val type: MessageType,
-    val value: Struct,
+    val type: String,
+    val value: String,
 ) {
     companion object {
         fun fromMessage(message: Message): SerializableMessage {
-            return SerializableMessage(message.from, message.type, message.value)
+            return SerializableMessage(message.from, message.type.toString(), message.value.toString())
+        }
+
+        fun toMessage(serializableMessage: SerializableMessage): Message {
+            val type = when (serializableMessage.type) {
+                Achieve.toString() -> Achieve
+                Tell.toString() -> Tell
+                else -> throw Exception()
+            }
+            return Message(serializableMessage.from, type, Struct.parse(serializableMessage.value))
         }
     }
 }
