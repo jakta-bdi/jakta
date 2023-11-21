b27de8ec28bde7380f4de67379371f629c5d349a
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-11-21 15:47:42 +0000

    refactor: now messages are serialized

--- jakta-distributed/jakta-distributed-adapter/src/main/kotlin/it/unibo/jakta/agents/distributed/network/impl/WebsocketNetwork.kt
+++ jakta-distributed/jakta-distributed-adapter/src/main/kotlin/it/unibo/jakta/agents/distributed/network/impl/WebsocketNetwork.kt
@@ -4,6 +4,7 @@ import it.unibo.jakta.agents.bdi.actions.effects.EnvironmentChange
 import it.unibo.jakta.agents.bdi.actions.effects.SendMessage
 import it.unibo.jakta.agents.distributed.RemoteService
 import it.unibo.jakta.agents.distributed.client.Client
+import it.unibo.jakta.agents.distributed.common.SerializableSendMessage
 import it.unibo.jakta.agents.distributed.network.Network
 
 class WebsocketNetwork(host: String, port: Int) : Network {
@@ -13,7 +14,7 @@ class WebsocketNetwork(host: String, port: Int) : Network {
     }
 
     override suspend fun send(event: SendMessage) {
-        client.publish(event.recipient, event)
+        client.publish(event.recipient, SerializableSendMessage.fromSendMessage(event))
     }
     override fun getMessagesAsEnvironmentChanges(): Iterable<EnvironmentChange> =
         client.incomingData.values.asIterable().map { it as EnvironmentChange }
