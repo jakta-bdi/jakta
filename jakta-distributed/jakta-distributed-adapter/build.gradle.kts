0e3cfbad0a3a3bbcf03083bb9260e003dc8bf225
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-11-21 13:05:54 +0000

    chore(deps): add kotlinx-coroutines

--- jakta-distributed/jakta-distributed-adapter/build.gradle.kts
+++ jakta-distributed/jakta-distributed-adapter/build.gradle.kts
@@ -11,4 +11,5 @@ dependencies {
     implementation(libs.logback.classic)
     implementation(project(mapOf("path" to ":jakta-bdi")))
     implementation(project(mapOf("path" to ":jakta-distributed:jakta-distributed-client")))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
 }
