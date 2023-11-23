93337af742bc1d52b0711475977c8fd1e2c00749
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-11-23 09:58:06 +0000

    feat: now broker service manages exceptions and logging is improved

--- jakta-distributed/jakta-distributed-broker/src/main/kotlin/it/unibo/jakta/agents/distributed/broker/plugins/WebSockets.kt
+++ jakta-distributed/jakta-distributed-broker/src/main/kotlin/it/unibo/jakta/agents/distributed/broker/plugins/WebSockets.kt
@@ -3,6 +3,7 @@ package it.unibo.jakta.agents.distributed.broker.plugins
 import io.ktor.serialization.kotlinx.KotlinxWebsocketSerializationConverter
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
+import io.ktor.server.plugins.origin
 import io.ktor.server.routing.routing
 import io.ktor.server.websocket.WebSockets
 import io.ktor.server.websocket.pingPeriod
@@ -11,6 +12,7 @@ import io.ktor.server.websocket.webSocket
 import io.ktor.websocket.Frame
 import it.unibo.jakta.agents.distributed.broker.model.Error
 import it.unibo.jakta.agents.distributed.broker.model.SubscriptionManager
+import kotlinx.coroutines.channels.ClosedReceiveChannelException
 import kotlinx.serialization.json.Json
 import java.time.Duration
 
@@ -28,38 +30,74 @@ fun Application.configureWebSockets(subscriptionManager: SubscriptionManager) {
 
         webSocket("/publish/{topic}") {
             val topic = call.parameters["topic"] ?: ""
-            call.application.environment.log.info("New publish channel open: $topic")
+            call.application.environment.log.info(
+                "${call.request.origin.remoteAddress}:${call.request.origin.remotePort} now publishes on $topic"
+            )
             subscriptionManager.addPublisher(this, topic)
-            for (frame in incoming) {
-                subscriptionManager.subscribers(topic)
-                    .forEach { it.send(frame.copy()) }
+            try {
+                for (frame in incoming) {
+                    subscriptionManager.subscribers(topic)
+                        .forEach { it.send(frame.copy()) }
+                }
+            } catch (e: ClosedReceiveChannelException) {
+                call.application.environment.log.info("onClose ${closeReason.await()}")
+            } catch (e: Throwable) {
+                call.application.environment.log.error("onError ${closeReason.await()}")
+                e.printStackTrace()
+            } finally {
+                call.application.environment.log.info(
+                    "Removing ${call.request.origin.remoteAddress}:${call.request.origin.remotePort}"
+                )
+                subscriptionManager.removePublisher(this, topic)
             }
-            call.application.environment.log.info("Removing $this")
-            subscriptionManager.removePublisher(this, topic)
         }
 
         webSocket("/subscribe/{topic}") {
             val topic = call.parameters["topic"] ?: ""
-            call.application.environment.log.info("New subscription on: $topic")
+            call.application.environment.log.info(
+                "${call.request.origin.remoteAddress}:${call.request.origin.remotePort} subscribed on $topic"
+            )
             subscriptionManager.addSubscriber(this, topic)
-            for (frame in incoming) {
-                this.send(Frame.Text(Error.BAD_REQUEST.toString()))
+            try {
+                for (frame in incoming) {
+                    this.send(Frame.Text(Error.BAD_REQUEST.toString()))
+                }
+            } catch (e: ClosedReceiveChannelException) {
+                call.application.environment.log.info("onClose ${closeReason.await()}")
+            } catch (e: Throwable) {
+                call.application.environment.log.error("onError ${closeReason.await()}")
+                e.printStackTrace()
+            } finally {
+                call.application.environment.log.info(
+                    "Removing ${call.request.origin.remoteAddress}:${call.request.origin.remotePort}"
+                )
+                subscriptionManager.removeSubscriber(this, topic)
             }
-            call.application.environment.log.info("Removing $this")
-            subscriptionManager.removeSubscriber(this, topic)
         }
 
         webSocket("/subscribe-all/{except...}") {
-            call.application.environment.log.info("New subscription: $this")
+            call.application.environment.log.info(
+                "New subscribe-all from ${call.request.origin.remoteAddress}:${call.request.origin.remotePort}"
+            )
             val except = call.parameters.getAll("except") ?: emptyList()
             subscriptionManager.availableTopics().minus(except.toSet())
                 .forEach { subscriptionManager.addSubscriber(this, it) }
-            for (frame in incoming) {
-                this.send(Frame.Text(Error.BAD_REQUEST.toString()))
+            try {
+                for (frame in incoming) {
+                    this.send(Frame.Text(Error.BAD_REQUEST.toString()))
+                }
+            } catch (e: ClosedReceiveChannelException) {
+                call.application.environment.log.info("onClose ${closeReason.await()}")
+            } catch (e: Throwable) {
+                call.application.environment.log.error("onError ${closeReason.await()}")
+                e.printStackTrace()
+            } finally {
+                call.application.environment.log.info(
+                    "Removing ${call.request.origin.remoteAddress}:${call.request.origin.remotePort}"
+                )
+                subscriptionManager.availableTopics().minus(except.toSet())
+                    .forEach { subscriptionManager.removeSubscriber(this, it) }
             }
-            call.application.environment.log.info("Removing $this")
-            subscriptionManager.availableTopics().minus(except.toSet())
-                .forEach { subscriptionManager.removeSubscriber(this, it) }
         }
     }
 }
