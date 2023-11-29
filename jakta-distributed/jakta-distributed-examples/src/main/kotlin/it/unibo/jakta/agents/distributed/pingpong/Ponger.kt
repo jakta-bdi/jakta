2cf295dba8d8ba056d20addfe660506213658101
LEONARDO MICELLI <leonardomicelli@gmail.com>
2023-11-29 15:33:47 +0100

    feat: implement a DMas with embedded broker

--- jakta-distributed/jakta-distributed-examples/src/main/kotlin/it/unibo/jakta/agents/distributed/pingpong/Ponger.kt
+++ jakta-distributed/jakta-distributed-examples/src/main/kotlin/it/unibo/jakta/agents/distributed/pingpong/Ponger.kt
@@ -13,61 +13,50 @@ import it.unibo.jakta.agents.bdi.goals.UpdateBelief
 import it.unibo.jakta.agents.bdi.plans.Plan
 import it.unibo.jakta.agents.bdi.plans.PlanLibrary
 import it.unibo.jakta.agents.distributed.RemoteService
-import it.unibo.jakta.agents.distributed.broker.embedded.EmbeddedBroker
 import it.unibo.jakta.agents.distributed.dmas.DMas
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 
 fun main() {
-    runBlocking {
-        val embeddedServer = EmbeddedBroker(8080)
-        launch(Dispatchers.Default) { embeddedServer.start() }
-        delay(3000)
+    val env = Environment.of(
+        externalActions = mapOf(
+            sendAction.signature.name to sendAction,
+        ),
+    )
 
-        val env = Environment.of(
-            externalActions = mapOf(
-                sendAction.signature.name to sendAction,
-            ),
-        )
-
-        val ponger = Agent.of(
-            name = "ponger",
-            beliefBase = BeliefBase.of(
-                Belief.fromSelfSource(Jakta.parseStruct("turn(other)")),
-                Belief.fromSelfSource(Jakta.parseStruct("other(pinger)")),
-            ),
-            planLibrary = PlanLibrary.of(
-                Plan.ofBeliefBaseAddition(
-                    belief = Belief.from(Jakta.parseStruct("ball(source(Sender))")),
-                    guard = Jakta.parseStruct("turn(source(self), other) & other(source(self), Sender)"),
-                    goals = listOf(
-                        UpdateBelief.of(Belief.fromSelfSource(Jakta.parseStruct("turn(me)"))),
-                        RemoveBelief.of(Belief.from(Jakta.parseStruct("ball(source(Sender))"))),
-                        ActInternally.of(Jakta.parseStruct("print(\"Received ball from\", Sender)")),
-                        Achieve.of(Jakta.parseStruct("sendMessageTo(ball, Sender)")),
-                        Achieve.of(Jakta.parseStruct("handle_ping")),
-                    ),
+    val ponger = Agent.of(
+        name = "ponger",
+        beliefBase = BeliefBase.of(
+            Belief.fromSelfSource(Jakta.parseStruct("turn(other)")),
+            Belief.fromSelfSource(Jakta.parseStruct("other(pinger)")),
+        ),
+        planLibrary = PlanLibrary.of(
+            Plan.ofBeliefBaseAddition(
+                belief = Belief.from(Jakta.parseStruct("ball(source(Sender))")),
+                guard = Jakta.parseStruct("turn(source(self), other) & other(source(self), Sender)"),
+                goals = listOf(
+                    UpdateBelief.of(Belief.fromSelfSource(Jakta.parseStruct("turn(me)"))),
+                    RemoveBelief.of(Belief.from(Jakta.parseStruct("ball(source(Sender))"))),
+                    ActInternally.of(Jakta.parseStruct("print(\"Received ball from\", Sender)")),
+                    Achieve.of(Jakta.parseStruct("sendMessageTo(ball, Sender)")),
+                    Achieve.of(Jakta.parseStruct("handle_ping")),
                 ),
-                Plan.ofAchievementGoalInvocation(
-                    value = Jakta.parseStruct("handle_ping"),
-                    goals = listOf(
-                        UpdateBelief.of(Belief.fromSelfSource(Jakta.parseStruct("turn(other)"))),
-                        ActInternally.of(Jakta.parseStruct("print(\"Ponger has Done\")")),
-                    ),
-                ),
-                sendPlan,
             ),
-        )
+            Plan.ofAchievementGoalInvocation(
+                value = Jakta.parseStruct("handle_ping"),
+                goals = listOf(
+                    UpdateBelief.of(Belief.fromSelfSource(Jakta.parseStruct("turn(other)"))),
+                    ActInternally.of(Jakta.parseStruct("print(\"Ponger has Done\")")),
+                ),
+            ),
+            sendPlan,
+        ),
+    )
 
-        val pinger = RemoteService("pinger")
-        val dmas = DMas.withEmbeddedBroker(
-            ExecutionStrategy.oneThreadPerAgent(),
-            env,
-            listOf(ponger),
-            listOf(pinger),
-        )
-        dmas.start()
-    }
+    val pinger = RemoteService("pinger")
+
+    DMas.withEmbeddedBroker(
+        ExecutionStrategy.oneThreadPerAgent(),
+        env,
+        listOf(ponger),
+        listOf(pinger),
+    ).start()
 }
