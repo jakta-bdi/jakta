7da00722ff5db8fc919b153c14cb1068950eb8c6
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-12-04 15:35:11 +0000

    chore(deps): add jakta-dsl dependency

--- jakta-distributed/jakta-distributed-examples/build.gradle.kts
+++ jakta-distributed/jakta-distributed-examples/build.gradle.kts
@@ -1,3 +1,6 @@
+import org.gradle.configurationcache.extensions.capitalized
+import java.nio.charset.Charset
+
 plugins {
     application
     alias(libs.plugins.kotlin.jvm)
@@ -13,6 +16,7 @@ dependencies {
     implementation(project(mapOf("path" to ":jakta-bdi")))
     implementation(project(mapOf("path" to ":jakta-distributed:jakta-distributed-adapter")))
     implementation(project(mapOf("path" to ":jakta-distributed:jakta-distributed-broker")))
+    implementation(project(mapOf("path" to ":jakta-dsl")))
 }
 
 tasks.register<JavaExec>("runPonger") {
@@ -23,4 +27,32 @@ tasks.register<JavaExec>("runPonger") {
 tasks.register<JavaExec>("runPinger") {
     mainClass = "it.unibo.jakta.agents.distributed.pingpong.PingerKt"
     classpath = sourceSets.main.get().runtimeClasspath
+}
+
+fun mainFiles() = project
+    .projectDir
+    .listFiles { f: File -> f.name == "src" }
+    ?.firstOrNull()
+    ?.walk()
+    ?.filter { it.isFile && it.readText(Charset.defaultCharset()).contains("fun main") }
+    // ?.map { it.nameWithoutExtension }
+    ?.toList()
+    ?: emptyList()
+
+fun fromPathToClasspath(file: File) = file
+    .path
+    .replaceBefore(fromDotToSeparator(project.group.toString()), "")
+    .dropLast(3)
+    .fromSeparatorToDot()
+
+fun fromDotToSeparator(string: String) = string.replace('.', File.separatorChar)
+fun String.fromSeparatorToDot() = this.replace(File.separatorChar, '.')
+
+mainFiles().forEach {
+    task<JavaExec>("jakta${it.nameWithoutExtension.capitalized()}") {
+        group = "JaKtA examples"
+        sourceSets.main { classpath = runtimeClasspath }
+        mainClass.set(fromPathToClasspath(it))
+        standardInput = System.`in`
+    }
 }
