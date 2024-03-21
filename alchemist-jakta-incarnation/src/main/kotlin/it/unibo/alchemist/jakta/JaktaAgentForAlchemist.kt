d2500752620291d982499feca6e5889402c51e7c
Danilo Pianini <danilo.pianini@unibo.it>
2024-03-21 11:09:30 +0100

    feat: implement loading based on string and arbitrary arguments

--- alchemist-jakta-incarnation/src/main/kotlin/it/unibo/alchemist/jakta/JaktaAgentForAlchemist.kt
+++ alchemist-jakta-incarnation/src/main/kotlin/it/unibo/alchemist/jakta/JaktaAgentForAlchemist.kt
@@ -4,16 +4,27 @@ import it.unibo.alchemist.model.Action
 import it.unibo.alchemist.model.Context
 import it.unibo.alchemist.model.Dependency
 import it.unibo.alchemist.model.Node
+import it.unibo.alchemist.model.Node.Companion.asProperty
 import it.unibo.alchemist.model.Position
 import it.unibo.alchemist.model.Reaction
 import it.unibo.alchemist.model.actions.AbstractAction
 import it.unibo.jakta.agents.bdi.Agent
 import it.unibo.jakta.agents.bdi.AgentLifecycle
+import kotlin.reflect.KCallable
 
 class JaktaAgentForAlchemist<P : Position<P>>(
     val jaktaEnvironment: JaktaEnvironmentForAlchemist<P>,
     val agent: Agent,
 ) : AbstractAction<Any?>(jaktaEnvironment.node) {
+
+    constructor(
+        node: Node<Any?>,
+        agentFactory: String,
+        parameters: List<Any?>,
+    ) : this(
+        node.asProperty<Any?, JaktaEnvironmentForAlchemist<P>>(),
+        agentFactory.reflectMethod().call(*parameters.toTypedArray()),
+    )
 
     init {
         declareDependencyTo(Dependency.EVERYTHING)
@@ -31,4 +42,14 @@ class JaktaAgentForAlchemist<P : Position<P>>(
     }
 
     override fun getContext() = Context.GLOBAL
+
+    companion object {
+        fun String.reflectMethod(): KCallable<Agent> {
+            val method = substringAfterLast('.')
+            val `class` = substringBeforeLast('.')
+            return Class.forName(`class`).kotlin.members
+                .filterIsInstance<KCallable<Agent>>()
+                .first { it.name == method }
+        }
+    }
 }
