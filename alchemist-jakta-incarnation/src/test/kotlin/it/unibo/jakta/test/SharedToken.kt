6f72a9e99379aee73eed7fb5f54dd98a1cbf7cb6
Martina Baiardi <m.baiardi@unibo.it>
2024-03-22 17:53:11 +0100

    fix: improvement of jakta incarnation implementation and test refinement

--- alchemist-jakta-incarnation/src/test/kotlin/it/unibo/jakta/test/SharedToken.kt
+++ alchemist-jakta-incarnation/src/test/kotlin/it/unibo/jakta/test/SharedToken.kt
@@ -3,7 +3,6 @@
 package it.unibo.jakta.test
 
 import it.unibo.alchemist.jakta.JaktaEnvironmentForAlchemist
-import it.unibo.alchemist.jakta.fix
 import it.unibo.alchemist.model.Node
 import it.unibo.alchemist.model.Position
 import it.unibo.alchemist.model.molecules.SimpleMolecule
@@ -27,15 +26,13 @@ fun <P : Position<P>> JaktaEnvironmentForAlchemist<P>.entrypoint(): Agent {
 fun <P : Position<P>> JaktaEnvironmentForAlchemist<P>.entrypointWithColor(color: Color): Agent {
     return tokenPassAgent("Agent#${color.rgb.toHexString()}@${node.id}", color)
 }
+data class ColoredAgent(val name: String, val location: Node<Any?>, val color: Color)
 
 fun <P : Position<P>> JaktaEnvironmentForAlchemist<P>.tokenPassAgent(name: String, color: Color): Agent =
     with(AgentScope(name)) {
-        data class ColoredAgent(val name: String, val location: Node<Any?>, val color: Color)
         val myColor = ColoredAgent(name, node, color)
         beliefs {
-            fact {
-                "myColor"(ObjectRef.of(color))
-            }
+            fact("myColor"(ObjectRef.of(color)))
         } // [myColor(source(self), $color), ball(source(percept), nodeX, red), knownAgents(source(percept),
         // [ColoredAgent("foo", red)])
         goals {
@@ -43,32 +40,54 @@ fun <P : Position<P>> JaktaEnvironmentForAlchemist<P>.tokenPassAgent(name: Strin
         }
         plans {
             +achieve("init") then {
-                execute(run, {
-                    val listOfAgents = node.getConcentration(knownAgents) as? Set<*>
-
-                    // Cannot convert class it.unibo.jakta.test.SharedToken$tokenPassAgent$1$ColoredAgent into class it.unibo.tuprolog.core.Term
-                    fun Node<Any?>.update() = setConcentration(
-                        knownAgents,
-                        arrayListOf<Any>(myColor).also { it.addAll(listOfAgents.orEmpty().filterNotNull()) },
-                    )
-                    alchemistEnvironment.getNeighborhood(node).forEach {
-                        it.update()
-                    }
-                    node.update()
-                })
+                execute("writeData"(name, ObjectRef.of(myColor)))
+//                execute(run, {
+//                    val listOfAgents = node.getConcentration(knownAgents) as? Set<*>
+//
+//                    fun Node<Any?>.update() = setConcentration(
+//                        knownAgents,
+//                        arrayListOf<Any>(myColor).also { it.addAll(listOfAgents.orEmpty().filterNotNull()) },
+//                    )
+//                    alchemistEnvironment.getNeighborhood(node).forEach {
+//                        it.update()
+//                    }
+//                    node.update()
+//                })
             }
 
             +"ball"(`_`).fromPercept onlyIf {
-                "myColor"(X).fromSelf and "ball"(X).fromPercept and A `is` knownAgents.name(A).fromPercept
+                "myColor"(X).fromSelf and "ball"(X).fromPercept
             } then {
                 execute(run, {
-                    val known = A.fix<Set<ColoredAgent>>() subtract setOf(myColor)
-                    if (known.isNotEmpty()) {
-                        val (_, destination, newColor) = known.randomElement(randomGenerator)
+                    println(data)
+                    val colors = data.filter { it.key.startsWith("Agent") }.values
+                        .filterIsInstance<ColoredAgent>()
+                        .subtract(setOf(myColor))
+
+                    // val known = A.fix<Set<ColoredAgent>>() subtract setOf(myColor)
+                    if (colors.isNotEmpty()) {
+                        val (_, destination, newColor) = colors.randomElement(randomGenerator)
+                        removeData("ball")
                         destination.setConcentration(SimpleMolecule("ball"), newColor)
                         println("sent ball to $destination with color $newColor")
+                    } else {
+                        println("Why is it empty?")
                     }
                 })
             }
         }.build()
     }
+
+// fun entypoint(environmentForAlchemist: JaktaEnvironmentForAlchemist): Alc =
+//    jaktaLocality {
+//        environment(OnweAget) {
+//            actions {
+//                action("writeData") {
+//                    setData(argument[0], argument[1])
+//                }
+//            }
+//        }
+//        agens {
+//
+//        }
+//    }
