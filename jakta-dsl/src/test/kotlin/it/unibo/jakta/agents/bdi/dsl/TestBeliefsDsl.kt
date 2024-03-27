8b69ea7783d0969aa54d6df5c735cc3a9d0cd0e7
Martina Baiardi <m.baiardi@unibo.it>
2024-03-27 16:15:30 +0100

    fix(DSL): fixed casting error in agent's belief base creation

--- jakta-dsl/src/test/kotlin/it/unibo/jakta/agents/bdi/dsl/TestBeliefsDsl.kt
+++ jakta-dsl/src/test/kotlin/it/unibo/jakta/agents/bdi/dsl/TestBeliefsDsl.kt
@@ -1,7 +1,11 @@
 package it.unibo.jakta.agents.bdi.dsl
 
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.collections.shouldContain
 import io.kotest.matchers.shouldBe
+import it.unibo.jakta.agents.bdi.beliefs.Belief
+import it.unibo.tuprolog.core.Atom
+import it.unibo.tuprolog.core.Struct
 
 class TestBeliefsDsl : DescribeSpec({
     describe("An agent beliefs") {
@@ -17,6 +21,46 @@ class TestBeliefsDsl : DescribeSpec({
             }
             val bb = mas.agents.first().context.beliefBase
             bb.count() shouldBe 3
+        }
+        it("can be described through methods") {
+            val mas = mas {
+                agent("test") {
+                    beliefs {
+                        fact("turn"("me"))
+                        fact("other"("ponger"))
+                        rule("s"("nat"(X)) impliedBy "s"(X))
+                    }
+                }
+            }
+            val bb = mas.agents.first().context.beliefBase
+            bb.count() shouldBe 3
+        }
+    }
+    describe("Agents Facts") {
+        it("can be declared using LogicProgrammingScope function as Atoms") {
+            val mas = mas {
+                agent("fact with fun") {
+                    beliefs {
+                        fact { "pippo" }
+                    }
+                }
+            }
+            val bb = mas.agents.first().context.beliefBase
+            bb shouldContain Belief.fromSelfSource(Atom.of("pippo"))
+            bb shouldContain Belief.fromSelfSource(Struct.of("pippo"))
+        }
+
+        it("can be declared passing Atoms as arguments") {
+            val mas = mas {
+                agent("fact with args") {
+                    beliefs {
+                        fact("pippo")
+                    }
+                }
+            }
+            val bb = mas.agents.first().context.beliefBase
+            bb shouldContain Belief.fromSelfSource(Atom.of("pippo"))
+            bb shouldContain Belief.fromSelfSource(Struct.of("pippo"))
         }
     }
 })
