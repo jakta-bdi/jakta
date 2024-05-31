cf80772aa2dbbf50ce860b16c7ab17c4ebf3572e
Martina <39519366+anitvam@users.noreply.github.com>
2024-05-31 19:58:09 +0200

    perf: performance boost

--- jakta-bdi/src/main/kotlin/it/unibo/jakta/agents/bdi/beliefs/impl/BeliefBaseImpl.kt
+++ jakta-bdi/src/main/kotlin/it/unibo/jakta/agents/bdi/beliefs/impl/BeliefBaseImpl.kt
@@ -19,13 +19,6 @@ internal class BeliefBaseImpl private constructor(private val beliefs: ClauseMul
 
     constructor() : this(ClauseMultiSet.empty(Unificator.default))
 
-    private val operatorExtension = Theory.of(
-        Jakta.parseClause("&(A, B) :- A, B"),
-        Jakta.parseClause("|(A, _) :- A"),
-        Jakta.parseClause("|(_, B) :- B"),
-        Jakta.parseClause("~(X) :- not(X)"),
-    )
-
     override fun add(belief: Belief) = when (beliefs.count(belief.rule)) {
         // There's no Belief that unify the param inside the MultiSet, so it's inserted
         0L -> RetrieveResult(listOf(BeliefUpdate.addition(belief)), BeliefBaseImpl(beliefs.add(belief.rule)))
@@ -97,4 +90,13 @@ internal class BeliefBaseImpl private constructor(private val beliefs: ClauseMul
 
     override fun toString(): String =
         beliefs.joinToString { Belief.from(it.castToRule()).toString() }
+
+    companion object {
+        private val operatorExtension = Theory.of(
+            Jakta.parseClause("&(A, B) :- A, B"),
+            Jakta.parseClause("|(A, _) :- A"),
+            Jakta.parseClause("|(_, B) :- B"),
+            Jakta.parseClause("~(X) :- not(X)"),
+        )
+    }
 }
