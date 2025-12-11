4b358c3b6f9870d619358748bd4263d9fcf9b1db
Samuele Burattini <samuele.burattini@unibo.it>
2025-12-11 13:05:07 +0100

    wip: skills with comments and better example

--- jakta-api/src/commonMain/kotlin/it/unibo/jakta/environment/PerceivingEnvironment.kt
+++ jakta-api/src/commonMain/kotlin/it/unibo/jakta/environment/PerceivingEnvironment.kt
@@ -1,8 +1,11 @@
 package it.unibo.jakta.environment
 
 import it.unibo.jakta.agent.Agent
+import it.unibo.jakta.environment.Movement.Events.position
+import it.unibo.jakta.environment.Recharging.Events.chargeLevel
 import it.unibo.jakta.event.Event
 import kotlinx.coroutines.channels.SendChannel
+import kotlinx.coroutines.delay
 
 /**
  * Environment which listens for external events and is capable to forward them to agents.
@@ -39,35 +42,66 @@ class PerceivingEnvironment<PerceptionPayload : Any, Belief : Any, Goal : Any>(
     }
 }
 
+//This will become the actual Environment interface
+interface BrokerEnvironment : Environment {
+    val broker : SendChannel<Event.External.Perception<out BrokerEnvironment, *>>
+}
 
-val broker : SendChannel<Event.External> = TODO()
-
-interface SpatialEnvironment<Position> : Environment {
+interface SpatialEnvironment<Position> : BrokerEnvironment {
     val position: Position
 }
 
+//concrete implementation of an environment
 data class GridEnvironment(override val position: Pair<Int, Int>)
-    : SpatialEnvironment<Pair<Int, Int>> {}
+    : SpatialEnvironment<Pair<Int, Int>> {
+        override val broker : SendChannel<Event.External.Perception<out BrokerEnvironment, *>> = TODO()
+    }
 
+//Interface for skills, skills depend on an environment to be able to send perceptions
+//and possibly interact with environment-specific features
 interface Skill<Env: Environment> {
     val environment: Env
 }
 
-interface Cleaning : Skill<Environment> {
-    fun clean()
+//This is a skill that depends on a generic environment, i.e. it does not need any specific feature
+//apart from the broker to notify the agent of charge complete
+//Lets the agent recharge its battery
+interface Recharging : Skill<BrokerEnvironment> {
+    suspend fun recharge()
+
+    //factory for events of Recharging
+    //receiver prevents usage from outside Recharging extensions
+    object Events {
+        fun Recharging.chargeLevel(level: Int): ChargeLevel =
+            ChargeLevel(level)
+    }
 }
 
+data class ChargeLevel internal constructor(val level: Int) :
+    Event.External.Perception<BrokerEnvironment, Recharging>
+
+//Implementation of the actual skill for recharging
+data class FixedTimeRecharging(override val environment: BrokerEnvironment) : Recharging {
+    override suspend fun recharge() {
+        println("Recharging...")
+        delay(3000)
+        println("Recharged!")
+        environment.broker.trySend(this.chargeLevel(100))
+    }
+}
+
+//This is a generic movement skill that depends on an environment with some spatial nature
 interface Movement<P> : Skill<SpatialEnvironment<P>> {
     fun moveTo(direction: P) : P
-}
 
-
-data class CleaningImpl(override val environment: Environment) : Cleaning {
-    override fun clean() {
-        TODO("Not yet implemented")
+    object Events {
+        fun <P> Movement<P>.position(pos: P): Position<P> =
+            Position(pos)
     }
-
 }
+
+data class Position<P> internal constructor (val position: P) :
+    Event.External.Perception<SpatialEnvironment<P>, Movement<P>>
 
 
 data class GridMovement(override val environment: GridEnvironment) : Movement<Pair<Int, Int>> {
@@ -75,33 +109,18 @@ data class GridMovement(override val environment: GridEnvironment) : Movement<Pa
         val (x, y) = environment.position
         val (dx, dy) = direction
         val newPos =  (x+dx to y+dy)
-        broker.trySend(Position( newPos))
+        environment.broker.trySend(this.position(newPos))
         return newPos
+
     }
 }
-
-data class Position<P>(val position: P) :
-    Event.External.Perception<SpatialEnvironment<P>, Movement<P>>
-
 
 val myEnv = GridEnvironment(0 to 0 )
 
-//TODO OPS SI SPACCA QUI, ma c'ero quasi
-//object SkillSet :
-//    Cleaning by CleaningImpl(myEnv),
-//    Movement<Pair<Int, Int>> by GridMovement(myEnv)
-
-//SUBOTTIMO
-object MySkills : Cleaning, Movement<Pair<Int, Int>> {
-
-    override fun clean() {
-        CleaningImpl(myEnv).clean()
-    }
-
-    override fun moveTo(direction: Pair<Int, Int>): Pair<Int, Int> {
-        GridMovement(myEnv).moveTo(direction)
-    }
-
-    override val environment: SpatialEnvironment<Pair<Int, Int>>
-        get() = myEnv
+object SkillSet :
+    Recharging by FixedTimeRecharging(myEnv),
+    Movement<Pair<Int, Int>> by GridMovement(myEnv) {
+    override val environment: GridEnvironment = myEnv
 }
+
+
