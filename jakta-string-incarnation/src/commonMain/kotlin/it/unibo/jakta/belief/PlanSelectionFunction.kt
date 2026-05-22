7b50cc1d79b569a6c3160a0f994d668f76e84c6e
Danilo Pianini <danilo.pianini@unibo.it>
2026-05-22 16:01:46 +0200

    style: apply ktlint

--- jakta-string-incarnation/src/commonMain/kotlin/it/unibo/jakta/belief/PlanSelectionFunction.kt
+++ jakta-string-incarnation/src/commonMain/kotlin/it/unibo/jakta/belief/PlanSelectionFunction.kt
@@ -2,8 +2,7 @@ package it.unibo.jakta.belief
 
 fun String.ifGoalMatches(goal: String): Unit? = Unit.takeIf { this == goal }
 
-fun String.matchesRegex(regex: String): Boolean? =
-    when (Regex(regex).containsMatchIn(this)) {
-        false -> null
-        else -> true
-    }
+fun String.matchesRegex(regex: String): Boolean? = when (Regex(regex).containsMatchIn(this)) {
+    false -> null
+    else -> true
+}
