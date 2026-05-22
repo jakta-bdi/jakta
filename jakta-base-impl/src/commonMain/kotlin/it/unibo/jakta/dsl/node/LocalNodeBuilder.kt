e2eb810a1d572ad55e0bf77994d84bb4af742d3f
Danilo Pianini <danilo.pianini@unibo.it>
2026-05-22 15:58:57 +0200

    style: apply ktlint

--- jakta-base-impl/src/commonMain/kotlin/it/unibo/jakta/dsl/node/LocalNodeBuilder.kt
+++ jakta-base-impl/src/commonMain/kotlin/it/unibo/jakta/dsl/node/LocalNodeBuilder.kt
@@ -7,25 +7,23 @@ import it.unibo.jakta.node.LocalNode
 /**
  * Implementation of the MasBuilder interface.
  */
-open class LocalNodeBuilder<Skills : Any, Body : Any> :
-    NodeBuilder<Skills, Body, LocalNode<Body, Skills>> {
+open class LocalNodeBuilder<Skills : Any, Body : Any> : NodeBuilder<Skills, Body, LocalNode<Body, Skills>> {
 
     private val node = LocalNode<Body, Skills>()
 
     protected val agents = mutableListOf<AgentBuilder<*, *, Skills, Body>>()
 
-    override fun <Belief : Any, Goal : Any> agent(
-        block: AgentBuilder<Belief, Goal, Skills, Body>.() -> Unit
-    ) = buildAgent(null, block)
+    override fun <Belief : Any, Goal : Any> agent(block: AgentBuilder<Belief, Goal, Skills, Body>.() -> Unit) =
+        buildAgent(null, block)
 
     override fun <Belief : Any, Goal : Any> agent(
         name: String,
-        block: AgentBuilder<Belief, Goal, Skills, Body>.() -> Unit
+        block: AgentBuilder<Belief, Goal, Skills, Body>.() -> Unit,
     ) = buildAgent(name, block)
 
     private fun <Belief : Any, Goal : Any> buildAgent(
         name: String?,
-        block: AgentBuilder<Belief, Goal, Skills, Body>.() -> Unit
+        block: AgentBuilder<Belief, Goal, Skills, Body>.() -> Unit,
     ) {
         val agentBuilder = AgentBuilderImpl<Belief, Goal, Skills, Body>(name)
         val agent: AgentBuilder<Belief, Goal, Skills, Body> = agentBuilder.apply(block)
