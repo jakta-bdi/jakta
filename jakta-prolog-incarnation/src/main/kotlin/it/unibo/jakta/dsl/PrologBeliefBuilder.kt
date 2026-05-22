c5190f3a20cadbf4da5cf2ebb8f87a6fd410c344
Danilo Pianini <danilo.pianini@unibo.it>
2026-05-22 16:02:16 +0200

    style: apply ktlint

--- jakta-prolog-incarnation/src/main/kotlin/it/unibo/jakta/dsl/PrologBeliefBuilder.kt
+++ jakta-prolog-incarnation/src/main/kotlin/it/unibo/jakta/dsl/PrologBeliefBuilder.kt
@@ -5,14 +5,11 @@ import it.unibo.tuprolog.core.Fact
 import it.unibo.tuprolog.core.Rule
 import it.unibo.tuprolog.core.Struct
 
-fun belief(block: JaktaLogicProgrammingScope.() -> Struct): Fact =
-    Fact.of(JaktaLogicProgrammingScope().block()).also {
-        require(it.isGround) { "Belief must be ground" }
-    }
+fun belief(block: JaktaLogicProgrammingScope.() -> Struct): Fact = Fact.of(JaktaLogicProgrammingScope().block()).also {
+    require(it.isGround) { "Belief must be ground" }
+}
 
-fun rule(block: JaktaLogicProgrammingScope.() -> Struct): Rule =
-    Rule.of(JaktaLogicProgrammingScope().block())
+fun rule(block: JaktaLogicProgrammingScope.() -> Struct): Rule = Rule.of(JaktaLogicProgrammingScope().block())
 
 context(scope: JaktaLogicProgrammingScope)
-fun beliefQuery(block: JaktaLogicProgrammingScope.() -> Struct): Fact =
-    Fact.of(scope.block())
+fun beliefQuery(block: JaktaLogicProgrammingScope.() -> Struct): Fact = Fact.of(scope.block())
