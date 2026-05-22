4f386243a5b2d69985eea533391d6f49cfd38d42
Danilo Pianini <danilo.pianini@unibo.it>
2026-05-22 15:51:18 +0200

    style: remove redundant blank

--- jakta-api/src/commonMain/kotlin/it/unibo/jakta/plan/GuardScope.kt
+++ jakta-api/src/commonMain/kotlin/it/unibo/jakta/plan/GuardScope.kt
@@ -3,7 +3,7 @@ package it.unibo.jakta.plan
 /**
  * Scope providing access to the beliefs relevant for evaluating a guard.
  */
-data class GuardScope<Belief : Any, PlanContext: Any>(
+data class GuardScope<Belief : Any, PlanContext : Any>(
     /**
      * The *beliefs* currently held by the agent.
      */
