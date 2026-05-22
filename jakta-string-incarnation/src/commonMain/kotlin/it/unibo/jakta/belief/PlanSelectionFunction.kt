989f3f6fa64fd49bad0e9db3ab93e34593b6b5e5
Danilo Pianini <danilo.pianini@unibo.it>
2026-05-22 16:01:33 +0200

    refactor: simplify ifGoalMatches implementation using `takeIf`

--- jakta-string-incarnation/src/commonMain/kotlin/it/unibo/jakta/belief/PlanSelectionFunction.kt
+++ jakta-string-incarnation/src/commonMain/kotlin/it/unibo/jakta/belief/PlanSelectionFunction.kt
@@ -1,7 +1,6 @@
 package it.unibo.jakta.belief
 
-fun String.ifGoalMatches(goal: String): Unit? =
-    if (this == goal) Unit else null
+fun String.ifGoalMatches(goal: String): Unit? = Unit.takeIf { this == goal }
 
 fun String.matchesRegex(regex: String): Boolean? =
     when (Regex(regex).containsMatchIn(this)) {
