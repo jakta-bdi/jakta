f61fe14df35c0f8aa77c2c69a34c150b0e4b889b
Danilo Pianini <danilo.pianini@unibo.it>
2026-05-22 15:49:03 +0200

    style: remove redundant blank

--- jakta-string-incarnation/src/commonMain/kotlin/it/unibo/jakta/belief/BeliefQueryingFunction.kt
+++ jakta-string-incarnation/src/commonMain/kotlin/it/unibo/jakta/belief/BeliefQueryingFunction.kt
@@ -2,5 +2,5 @@ package it.unibo.jakta.belief
 
 import it.unibo.jakta.plan.GuardScope
 
-fun <Context: Any> GuardScope<String, Context>.containsBeliefMatching(belief: String): Context? =
+fun <Context : Any> GuardScope<String, Context>.containsBeliefMatching(belief: String): Context? =
     if (this.beliefs.contains(belief)) this.context else null
